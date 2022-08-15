2334bf1a712b068b26531197ce6a6385dc0a3cc0
Shivam Negi <shivamnegi.2424@gmail.com>
2022-08-15 17:42:22 +0530

    v1.0.0

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ paytmParams['INDUSTRY_TYPE_ID'] =process.env.PAYTM_INDUSTRY_TYPE_ID;
 paytmParams['ORDER_ID'] =uuid();
 paytmParams['CUST_ID'] =process.env.PAYTM_CUST_ID;
 paytmParams['TXN_AMOUNT'] ='100';
-paytmParams['CALLBACK_URL'] ='https://honeycodesecommerce.herokuapp.com';
+paytmParams['CALLBACK_URL'] ='https://honeycodesecommerce.herokuapp.com/callback';
 paytmParams['EMAIL'] ='honey24@gmail.com';
 paytmParams['MOBILE_NO'] ='1234567890';
