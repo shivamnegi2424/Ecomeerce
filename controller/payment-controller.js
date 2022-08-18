32c86993e3be61d947d8c109d273c83677fc0355
Shivam Negi <shivamnegi.2424@gmail.com>
2022-08-18 13:41:05 +0530

    v1.0.0

--- controller/payment-controller.js
+++ controller/payment-controller.js
@@ -53,7 +53,7 @@ export const paytmResponse = (request,response) => {
 
                 post_res.on('end',function(){
                     let result = JSON.parse(res)
-                    response.redirect('')
+                    response.redirect('https://honeycodesecommerce.herokuapp.com')
                 })
             });
  
